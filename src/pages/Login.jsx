b497dbc67358da1d244deaf2a718a01391e41e5b
hasan <hasanakoz1986@gmail.com>
2022-11-02 22:22:08 +0300

    login, nav and router, user context done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [user, setUser] = useState({ email: "", password: "" });
+  // const [user, setUser] = useState({ email: "", password: "" });
 
+  //Consuming LoginContext
+  const { user, setUser } = useContext(LoginContext);
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
+    navigate(-1);
   };
+
+  console.log(user);
 
   return (
     <Container>
@@ -20,7 +28,7 @@ const Login = () => {
             type="email"
             placeholder="Enter your email"
             name="email"
-            value={user?.email}
+            value={user?.email || ""}
             onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
         </Form.Group>
@@ -31,7 +39,7 @@ const Login = () => {
             type="password"
             placeholder="Enter your password"
             name="password"
-            value={user?.password}
+            value={user?.password || ""}
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </Form.Group>
